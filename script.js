25c4523d1178f0c684ee0d73194b7faad2c99ec4
lifewinning <lifewinning@gmail.com>
2015-01-09 13:05:21 -0500

    things and things

--- script.js
+++ script.js
@@ -3,11 +3,18 @@
 var app=angular.module('app', ['ngSanitize']);
 
 app.controller('context', function($scope, $http){
+	$http.get('./csvs/article-titles.json').
+		success(function(data, status, headers, config){
+			$scope.titles = data;
+		}).
+		error(function(data, status, headers, config) {
+		      console.log(error)
+		});
 	$http.get('./csvs/data-with-script.json').
 		 success(function(data, status, headers, config) {
 		      $scope.documents = data;
 		    }).
 		    error(function(data, status, headers, config) {
-		      console.log('what have you done my god you fool')
+		      console.log(error)
 		    });
 });
